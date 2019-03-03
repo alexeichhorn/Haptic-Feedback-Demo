4c7206d406dd8362b22178a8150d1e01c5925913
Alexander Eichhorn <10730702+alexeichhorn@users.noreply.github.com>
2019-03-03 16:11:16 +0100

    Initial Commit

---
+++ Haptic Feedback Test WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Haptic Feedback Test WatchKit Extension
+//
+//  Created by Alexander Eichhorn on 03.03.19.
+//  Copyright © 2019 Losjet - Alexander Eichhorn. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
